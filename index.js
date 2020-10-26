25df02632bfb1478c00be7bdeab51badd11f8a8f
laetitia.doerks <laetitia.doerks@bluewin.ch>
2020-10-26 16:46:10 +0100

    query to dbpedia

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ const {v4: uuid} = require('uuid');
 const axios = require('axios').default;
 
 var sparqler = require('sparqling-star');
-var myquery = new sparqler.Query();
-var album = {
-    'type': 'dbo:Album',
-    'dbo:artist' : 'dbr:Eminem'
-};
-myquery.registerVariable( 'album', album );
 var sparqler = new sparqler.Client();
+
+var myquery = 'SELECT DISTINCT ?s ?p ?o WHERE { ?s ?p ?o } LIMIT 10';
 sparqler.send( myquery, function( error, data ) {
     console.log( data.results.bindings );
 });
+console.log('a');
+console.log(myquery);
+
 
 
 
