0884b270b3652ef8598d176027fd4bd2e2ba2be5
laetitia.doerks <laetitia.doerks@bluewin.ch>
2020-10-29 17:29:40 +0100

    requete long-lat

--- twsProject1_guide/index.js
+++ twsProject1_guide/index.js
@@ -10,7 +10,8 @@ const schemeHeader = "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n" +
 var sparqler = require('sparqling-star');
 var sparqler = new sparqler.Client();
 
-var myquery = 'SELECT DISTINCT ?s ?p ?o WHERE { ?s ?p ?o } LIMIT 10';
+// var myquery = 'SELECT DISTINCT ?s ?p ?o WHERE { ?s ?p ?o } LIMIT 10';
+var myquery = 'SELECT DISTINCT * WHERE { ?s geo:lat ?la . ?s geo:long ?lo . FILTER(?la>48.85 AND ?la<48.86 AND ?lo>2.351 AND ?lo<2.352) . } LIMIT 100';
 sparqler.send( myquery, function( error, data ) {
     console.log( data.results.bindings );
 });
