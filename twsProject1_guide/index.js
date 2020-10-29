03c4ad845353bb7c5b969c16ba33c77c57582285
laetitia.doerks <laetitia.doerks@bluewin.ch>
2020-10-29 15:15:33 +0100

    add schema de requete

--- twsProject1_guide/index.js
+++ twsProject1_guide/index.js
@@ -2,6 +2,11 @@ const parser = require('fast-xml-parser');
 const fs = require('fs');
 const {v4: uuid} = require('uuid');
 
+const schemeHeader = "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n" +
+    "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.\n" +
+    "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> . \n\n" +
+    "@prefix : <http://cui.unige.ch/> . \n\n"
+
 var sparqler = require('sparqling-star');
 var sparqler = new sparqler.Client();
 
