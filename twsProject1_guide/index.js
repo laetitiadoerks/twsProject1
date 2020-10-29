0c0cbbcd5bfb045b4223810eeece57e9843a68c7
laetitia.doerks <laetitia.doerks@bluewin.ch>
2020-10-29 15:21:27 +0100

    add query poi

--- twsProject1_guide/index.js
+++ twsProject1_guide/index.js
@@ -16,3 +16,15 @@ sparqler.send( myquery, function( error, data ) {
 });
 console.log('a');
 console.log(myquery);
+
+// 
+// PREFIX : <http://cui.unige.ch/>
+// prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+// prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+// prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+//
+// prefix cui: <http://cui.unige.ch/>
+//
+// select * where {
+// 	?s a cui:POI.
+// }
